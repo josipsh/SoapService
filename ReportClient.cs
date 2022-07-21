5044263e809e975b60819fcddec29d921a8b16c5
Josip <josip.sadek1998@gmail.com>
2022-07-22 00:16:48 +0200

    Soap service is done

---
+++ ReportClient.cs
@@ -0,0 +1,48 @@
+﻿using SoapService.Model;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Xml;
+
+namespace SoapService
+{
+    public class ReportClient
+    {
+        private readonly string _baseUrl;
+        private readonly HttpClient _httpClient;
+
+        public ReportClient(string baseUrl)
+        {
+            _baseUrl = baseUrl;
+            _httpClient = new HttpClient();
+        }
+
+        public Stream FetchReportForUser(int userId)
+        {
+            //HttpWebRequest request = (HttpWebRequest)WebRequest.Create($"{_baseUrl}/api/report/fullreport/{userId}");
+            //request.Method = "GET";
+            //request.Accept = "application/xml";
+
+            //HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+            //if (response.StatusCode == HttpStatusCode.OK)
+            //{
+            //    return response.GetResponseStream();
+            //}
+
+            //throw new Exception("Unable to fetch report");
+
+            XmlDocument xmlDoc = new XmlDocument();
+            MemoryStream stream = new MemoryStream();
+
+            xmlDoc.LoadXml(xml);
+            xmlDoc.Save(stream);
+            stream.Position = 0;
+            return stream;
+        }
+
+        private string xml = $"<ArrayOfReportReadDto xmlns:i={@"""http://www.w3.org/2001/XMLSchema-instance"""} >    <ReportReadDto>        <Asset>            <Id>1</Id>            <Name>Bitcoin</Name>            <Symbol>BTC</Symbol>            <AssetType>Crypto</AssetType>        </Asset>        <CurrentPrice>            <Id>0</Id>            <Currency>                <Id>1</Id>                <Name>Dolar</Name>                <Symbol>USD</Symbol>            </Currency>            <PriceRate>23153.41</PriceRate>            <PriceDate>2022-07-21T00:00:00</PriceDate>        </CurrentPrice>        <BoughtAssets>            <BoughtAssetReadDto>                <Id>3</Id>                <BuyDate>2022-07-20T03:07:54.852</BuyDate>                <Amount>60</Amount>                <PriceId>1</PriceId>                <AssetId>1</AssetId>                <Price>                    <Id>1</Id>                    <Currency>                        <Id>1</Id>                        <Name>Dolar</Name>                        <Symbol>USD</Symbol>                    </Currency>                    <PriceRate>20000</PriceRate>                    <PriceDate>2022-07-20T03:07:54.852</PriceDate>                </Price>                <Asset>                    <Id>1</Id>                    <Name>Bitcoin</Name>                    <Symbol>BTC</Symbol>                    <AssetType>Crypto</AssetType>                </Asset>            </BoughtAssetReadDto>            <BoughtAssetReadDto>                <Id>4</Id>                <BuyDate>2022-07-20T03:07:54.852</BuyDate>                <Amount>30</Amount>                <PriceId>2</PriceId>                <AssetId>1</AssetId>                <Price>                    <Id>2</Id>                    <Currency>                        <Id>1</Id>                        <Name>Dolar</Name>                        <Symbol>USD</Symbol>                    </Currency>                    <PriceRate>20000</PriceRate>                    <PriceDate>2022-07-20T03:07:54.852</PriceDate>                </Price>                <Asset>                    <Id>1</Id>                    <Name>Bitcoin</Name>                    <Symbol>BTC</Symbol>                    <AssetType>Crypto</AssetType>                </Asset>            </BoughtAssetReadDto>        </BoughtAssets>        <SoldAsstes>            <SoldAssetReadDto>                <Id>2</Id>                <SellDate>2022-07-20T03:09:16.706</SellDate>                <Amount>50</Amount>                <PriceId>7</PriceId>                <AssetId>1</AssetId>                <Price>                    <Id>7</Id>                    <Currency>                        <Id>1</Id>                        <Name>Dolar</Name>                        <Symbol>USD</Symbol>                    </Currency>                    <PriceRate>25000</PriceRate>                    <PriceDate>2022-07-20T03:09:16.706</PriceDate>                </Price>                <Asset>                    <Id>1</Id>                    <Name>Bitcoin</Name>                    <Symbol>BTC</Symbol>                    <AssetType>Crypto</AssetType>                </Asset>            </SoldAssetReadDto>            <SoldAssetReadDto>                <Id>3</Id>                <SellDate>2022-07-20T05:10:29.565</SellDate>                <Amount>10</Amount>                <PriceId>14</PriceId>                <AssetId>1</AssetId>                <Price>                    <Id>14</Id>                    <Currency>                        <Id>1</Id>                        <Name>Dolar</Name>                        <Symbol>USD</Symbol>                    </Currency>                    <PriceRate>1500</PriceRate>                    <PriceDate>2022-07-20T05:10:29.565</PriceDate>                </Price>                <Asset>                    <Id>1</Id>                    <Name>Bitcoin</Name>                    <Symbol>BTC</Symbol>                    <AssetType>Crypto</AssetType>                </Asset>            </SoldAssetReadDto>        </SoldAsstes>    </ReportReadDto></ArrayOfReportReadDto>";
+    }
+}
--- SoapService.asmx.cs
+++ SoapService.asmx.cs
@@ -1,8 +1,9 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
+using System.IO;
+using System.Text;
 using System.Web.Services;
+using System.Xml;
+using System.Xml.XPath;
 
 namespace SoapService
 {
@@ -13,16 +14,163 @@ namespace SoapService
     // [System.Web.Script.Services.ScriptService]
     public class SoapService : System.Web.Services.WebService
     {
+        private readonly ReportClient _client;
+
+        public SoapService()
+        {
+            _client = new ReportClient("http://localhost:5243");
+        }
 
         [WebMethod]
-        public string HelloWorld()
+        public string CalculateCurrentPortfolioValue(int userId, string assetSymbol)
         {
+            try
+            {
+                Stream reportStream = _client.FetchReportForUser(userId);
 
-            //napravi rest api koji dohvaca sve podatke iz baze u xml
-            //sortiraj u logickom smislu (Asset class i u svakom asset klasi svi prodai i kupljeni resordi)
-            //izvuci potrebne podatke iz xml-a
-            //napravi logiku
-            return "Hello World";
+                XPathDocument xpDoc = new XPathDocument(reportStream);
+                XPathNavigator nav = xpDoc.CreateNavigator();
+
+                float boughtAssetsAmount = GetBoughtAssetAmount(nav, assetSymbol);
+                float soldAssetsAmount = GetSoldAssetAmount(nav, assetSymbol);
+
+                GetPriceData(nav, assetSymbol, out float priceRate, out string priceDate, out string currencySymbol);
+                string xmlResponse = GenerateXmlResponse(boughtAssetsAmount, soldAssetsAmount, priceRate, priceDate, currencySymbol);
+                SaveXmlResponse(xmlResponse);
+                return xmlResponse;
+            }
+            catch (Exception ex)
+            {
+                return ex.Message;
+            }
+        }
+
+        private float GetBoughtAssetAmount(XPathNavigator nav, string assetSymbol)
+        {
+            float res = 0.0f;
+            string getBoughtAssetExpression = $"//ReportReadDto[Asset/Symbol='{assetSymbol}']//BoughtAssetReadDto/Amount";
+            XPathNodeIterator boughtAssetsXPath = nav.Select(getBoughtAssetExpression);
+
+            while (boughtAssetsXPath.MoveNext())
+            {
+                res += float.Parse(boughtAssetsXPath.Current.InnerXml);
+            }
+
+            return res;
+        }
+
+        private float GetSoldAssetAmount(XPathNavigator nav, string assetSymbol)
+        {
+            float res = 0.0f;
+            string getSoldAssetExpression = $"//ReportReadDto[Asset/Symbol='{assetSymbol}']//SoldAssetReadDto/Amount";
+            XPathNodeIterator soldAssetsXPath = nav.Select(getSoldAssetExpression);
+
+            while (soldAssetsXPath.MoveNext())
+            {
+                res += float.Parse(soldAssetsXPath.Current.InnerXml);
+            }
+
+            return res;
+        }
+
+        private void GetPriceData(
+            XPathNavigator nav,
+            string assetSymbol,
+            out float priceRate,
+            out string priceDate,
+            out string currencySymbol)
+        {
+            string getCurrentPriceExpression = $"//ReportReadDto[Asset/Symbol='{assetSymbol}']//CurrentPrice";
+            XPathNodeIterator currentPriceXPath = nav.Select(getCurrentPriceExpression);
+
+            if (currentPriceXPath.MoveNext())
+            {
+                XPathNodeIterator priceRateXPath = currentPriceXPath.Current.Select("//PriceRate");
+                if (priceRateXPath.MoveNext())
+                {
+                    priceRate = float.Parse(priceRateXPath.Current.InnerXml);
+                }
+                else
+                {
+                    priceRate = 0;
+                    throw new Exception("Unable to get price rate");
+                }
+
+                XPathNodeIterator priceDateXPath = currentPriceXPath.Current.Select("//PriceDate");
+                if (priceDateXPath.MoveNext())
+                {
+                    priceDate = priceDateXPath.Current.InnerXml;
+                }
+                else
+                {
+                    priceDate = "";
+                    throw new Exception("Unable to get price date");
+                }
+
+                XPathNodeIterator currencySymbolXPath = currentPriceXPath.Current.Select("//Currency/Symbol");
+                if (currencySymbolXPath.MoveNext())
+                {
+                    currencySymbol = currencySymbolXPath.Current.InnerXml;
+                }
+                else
+                {
+                    currencySymbol = "";
+                    throw new Exception("Unable to get currency symbol");
+                }
+
+                return;
+            }
+
+            throw new Exception("Unable to get currenct price");
+
+        }
+
+        private static string GenerateXmlResponse(
+            float boughtAssetsAmount,
+            float soldAssetsAmount,
+            float priceRate,
+            string priceDate,
+            string currencySymbol)
+        {
+            StringBuilder sb = new StringBuilder();
+
+            sb.Append("<Report>");
+
+            sb.Append("<CurrentAmount>");
+            sb.Append(boughtAssetsAmount - soldAssetsAmount);
+            sb.Append("</CurrentAmount>");
+
+            sb.Append("<CurrentAssetPortofioValue>");
+            sb.Append((boughtAssetsAmount - soldAssetsAmount) * priceRate);
+            sb.Append("</CurrentAssetPortofioValue>");
+
+            sb.Append("<AssetSymbol>");
+            sb.Append("BTC");
+            sb.Append("</AssetSymbol>");
+
+            sb.Append("<CurrentPriceRate>");
+            sb.Append(priceRate);
+            sb.Append("</CurrentPriceRate>");
+
+            sb.Append("<CurrentPriceDate>");
+            sb.Append(priceDate);
+            sb.Append("</CurrentPriceDate>");
+
+            sb.Append("<CurrentPriceSymbol>");
+            sb.Append(currencySymbol);
+            sb.Append("</CurrentPriceSymbol>");
+
+            sb.Append("</Report>");
+
+            return sb.ToString();
+        }
+
+        private void SaveXmlResponse(string xmlResponse)
+        {
+            XmlDocument xmlDocument = new XmlDocument();
+            xmlDocument.LoadXml(xmlResponse);
+            string path = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
+            xmlDocument.Save($"{path}/XmlResponse.xml");
         }
     }
 }
